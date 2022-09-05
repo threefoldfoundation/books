62d87bc697d79cfbc1d2bd42db221148a32467d7
despiegk <>
2022-09-05 16:58:15 +0200

    refactor

--- update.sh
+++ update.sh
@@ -2,12 +2,13 @@ set -ex
 
 source env.sh
 
-##WALK OVER THE BOOKS
-for d in books/*/ ; do
-    pushd $d
-    export DEST=$d && template     
-    popd
-done
+# ##WALK OVER THE BOOKS
+# for d in books/*/ ; do
+#     pushd $d
+#     export DEST=$d && template     
+#     popd
+# done
+
 
 
 echo "****DONE****"
